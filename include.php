40eb33067d9864786e4aa6218b5f06248d78c1de
Львович <Lvovich@users.noreply.github.com>
2024-03-12 19:41:25 +0200

    Init

---
+++ include.php
@@ -0,0 +1,7 @@
+<?php if(!defined('B_PROLOG_INCLUDED')||B_PROLOG_INCLUDED!==true)exit();
+
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadLanguageFile(__FILE__, 'ru');
+
+require_once __DIR__ . '/vendor/autoload.php';
