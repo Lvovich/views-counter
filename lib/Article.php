f80eb0e749c13f2513dcf55b64b8d68ce01b6a5c
Львович <Lvovich@users.noreply.github.com>
2024-03-18 22:36:30 +0200

    Article model.

---
+++ lib/Article.php
@@ -0,0 +1,114 @@
+<?php
+namespace Test\Views;
+
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\Error;
+use Bitrix\Main\ORM\Data\AddResult;
+use Bitrix\Main\ORM\Data\Result;
+use Bitrix\Main\ORM\Fields\ExpressionField;
+use Bitrix\Main\SystemException;
+use Exception;
+use Test\Views\DB\ArticleView;
+use Test\Views\DB\ArticleViewsTable;
+use Test\Views\DB\ViewsUser;
+
+class Article
+{
+    private int $articleId;
+
+    private int $viewsCount;
+
+    private ?ViewsUser $viewsUser = null;
+
+    /**
+     * @throws ArgumentException
+     */
+    public function __construct(int $elemId)
+    {
+        if (($this->articleId = $elemId) <= 0) {
+            throw new ArgumentException(GetMessage('TEST_ARTICLE_NOT_FOUND'));
+        }
+
+        if (!($this->viewsUser = ViewsUser::getInstance())) {
+            throw new ArgumentException(GetMessage('TEST_VIEWS_USER_NOT_FOUND'));
+        }
+
+        $this->viewsCount = $this->countViews();
+    }
+
+    public function addView(): AddResult
+    {
+        $res = new AddResult();
+
+        try {
+            $res = (new ArticleView())->setUserId($this->viewsUser->getId())->setElementId($this->articleId)->save();
+            $this->viewsCount++;
+        }
+        catch (Exception $e) {
+            $res->addError(new Error($e->getMessage()));
+        }
+
+        return $res;
+    }
+
+    public function hideView(): Result
+    {
+        $res = new Result();
+
+        try {
+            $filter = ['USER_ID'=>$this->viewsUser->getId(), 'ELEMENT_ID'=>$this->articleId];
+
+            if ($view = ArticleViewsTable::getList(['filter'=>$filter])->fetchObject()) {
+                $res = $view->delete();
+                $this->viewsCount--;
+            }
+        }
+        catch (Exception $e) {
+            $res->addError(new Error($e->getMessage()));
+        }
+
+        return $res;
+    }
+
+    public function clearViews(): Result
+    {
+        $res = new Result();
+
+        try {
+            $views = ArticleViewsTable::getList(['filter'=>['ELEMENT_ID'=>$this->articleId]])->fetchCollection();
+
+            foreach ($views as $view) {
+                $view->delete();
+            }
+
+            $res = $views->save();
+
+            $this->viewsCount = 0;
+        }
+        catch (Exception $e) {
+            $res->addError(new Error($e->getMessage()));
+        }
+
+        return $res;
+    }
+
+    public function getViewsCount(): int
+    {
+        return $this->viewsCount;
+    }
+
+    private function countViews(): int
+    {
+        try {
+            $res = ArticleViewsTable::getList([
+                'filter' => ['ELEMENT_ID'=>$this->articleId],
+                'select' => [new ExpressionField('CNT', 'COUNT(%s)', ['USER_ID'])]
+            ])->fetch();
+        }
+        catch (SystemException) {
+            $res = [];
+        }
+
+        return intval($res['CNT']);
+    }
+}
--- lib/DB/ViewsUser.php
+++ lib/DB/ViewsUser.php
@@ -1,6 +1,56 @@
 <?php
 namespace Test\Views\DB;
 
+use Bitrix\Main\Application;
+use Bitrix\Main\Context;
+use Bitrix\Main\Web\Cookie;
+use Exception;
+
 class ViewsUser extends EO_ViewsUsers
 {
+    private const COOKIE_NAME = 'VIEWS_USER';
+
+    private const COOKIE_EXP = '+1 year';
+
+    private const SALT = '9t%+mVdkueX(CI&m+y-G8|+#.dnT;d0K';
+
+    private static ?ViewsUser $instance = null; // change instance on data modification
+
+    public static function getInstance(): ViewsUser|null
+    {
+        if (!self::$instance) {
+            $context = Application::getInstance()->getContext();
+            $cookieId = $context->getRequest()->getCookie(self::COOKIE_NAME);
+
+            try {
+                self::$instance = ViewsUsersTable::getList(['filter'=>['COOKIE_ID'=>$cookieId]])->fetchObject();
+
+                if (!self::$instance) { // attempts to add a new user with unique cookieId
+                    $attempts = 3;
+
+                    do { // in a loop, since the identifier $newCookieId can repeat an existing one
+                        $newCookieId = md5(self::SALT . rand());
+
+                        self::setCookie($context, $newCookieId);
+                        self::$instance = (new self())->setCookieId($newCookieId);
+
+                    } while (!self::$instance->save()->isSuccess() && --$attempts>0);
+                }
+            }
+            catch (Exception) {
+            }
+        }
+
+        return self::$instance;
+    }
+
+    private static function setCookie(Context $context, string $newCookieId)
+    {
+        $cookie = (new Cookie(self::COOKIE_NAME, $newCookieId, strtotime(self::COOKIE_EXP)))
+            ->setPath('/')
+            ->setHttpOnly(false)
+            ->setDomain($context->getRequest()->getHttpHost());
+
+        $context->getResponse()->addCookie($cookie);
+    }
 }
